3e092e56e8b80a468b4a1e2824a2aeb672c8a3a4
Viqui Alaro <viquialaro241@gmail.com>
2021-12-27 21:44:42 -0400

    first commit

---
+++ src/main/java/com/mari/market/MariMarketApplication.java
@@ -0,0 +1,13 @@
+package com.mari.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mari/market/MariMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mari.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
