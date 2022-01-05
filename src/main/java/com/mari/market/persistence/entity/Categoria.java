17036d5c02c30a59d8ff0a9d35ef3ef5b0de7c0a
Viqui Alaro <viquialaro241@gmail.com>
2022-01-04 22:22:23 -0400

    se realizo el mapeo de relaciones entre clases

--- src/main/java/com/mari/market/persistence/entity/Categoria.java
+++ src/main/java/com/mari/market/persistence/entity/Categoria.java
@@ -1,19 +1,24 @@
 package com.mari.market.persistence.entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "categorias")
 public class Categoria {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_categoria")
     private Integer idCategoria;
 
     private String descripcion;
 
     private Boolean estado;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
+
 
 
     public Integer getIdCategoria() {
--- src/main/java/com/mari/market/persistence/entity/Compra.java
+++ src/main/java/com/mari/market/persistence/entity/Compra.java
@@ -2,13 +2,14 @@ package com.mari.market.persistence.entity;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "compras")
 public class Compra {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_compra")
     private Integer idCompra;
 
@@ -24,6 +25,12 @@ public class Compra {
 
     private String estado;
 
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", insertable = false,updatable = false)
+    private Cliente cliente;
+
+    @OneToMany(mappedBy = "producto")
+    private List<ComprasProducto> productos;
 
     public Integer getIdCompra() {
         return idCompra;
