c9f0c2ee5e9e74ed8d4ee331a5abf51c66b5fcec
Viqui Alaro <viquialaro241@gmail.com>
2022-01-07 20:28:23 -0400

    implementacion de Query metodos

--- src/main/java/com/mari/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/mari/market/persistence/crud/ProductoCrudRepository.java
@@ -3,7 +3,18 @@ package com.mari.market.persistence.crud;
 import com.mari.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
 
+    // esto es de forma nartiva
+   /* @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> getObtenerCategoria(int idCategoria);*/
+
+   /** Esto es con QUERY metodo*/
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 
 }
