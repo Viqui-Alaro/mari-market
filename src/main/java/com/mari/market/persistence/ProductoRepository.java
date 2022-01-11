bb8474f7120e63172afe0d9850fc963e2dd4a0cf
Viqui Alaro <viquialaro241@gmail.com>
2022-01-10 22:39:40 -0400

    implementacion de la anotación @service y se creo product service y los metodos como obtener todos, eliminar, guardar, etc..

--- src/main/java/com/mari/market/persistence/ProductoRepository.java
+++ src/main/java/com/mari/market/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.mari.market.domain.repository.ProductRepository;
 import com.mari.market.persistence.crud.ProductoCrudRepository;
 import com.mari.market.persistence.entity.Producto;
 import com.mari.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,7 +15,9 @@ import java.util.Optional;
 @Repository
 public class ProductoRepository implements ProductRepository {
 
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+    @Autowired
     private ProductMapper mapper;
 
     @Override
